092c313caf953cbcd2a25fbb7673cba5d2e91dda
EnyCode <69588272+EnyCode@users.noreply.github.com>
2024-07-25 17:12:23 +0100

    fix it lol

--- src/pages/upload.ts
+++ src/pages/upload.ts
@@ -1,7 +1,11 @@
 import type { APIRoute } from "astro"
+export const prerender = false;
 
 export const POST: APIRoute = async ({ request }) => {
-  console.log(request.body);
+  console.log(await request.json());
+
+  
+
   return new Response(JSON.stringify({
       message: "This was a POST!"
     })
