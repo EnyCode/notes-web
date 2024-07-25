8e620895f9f7e790c6182d60af21397db712c69a
EnyCode <69588272+EnyCode@users.noreply.github.com>
2024-07-25 19:51:21 +0100

    start parser

--- src/pages/upload.ts
+++ src/pages/upload.ts
@@ -1,13 +1,29 @@
 import type { APIRoute } from "astro"
+import {toMarkdown} from 'mdast-util-to-markdown'
+
 export const prerender = false;
 
 export const POST: APIRoute = async ({ request }) => {
-  console.log(await request.json());
-
-  
+  let rtf = await (await request.blob()).text();
+  //console.log(rtf);
+  parseRtf(rtf);
 
   return new Response(JSON.stringify({
       message: "This was a POST!"
     })
   )
+}
+
+const fontSize = {
+  "56": "h1"
+}
+
+function parseRtf(file: string) {
+  let lines = file.split("\n");
+  lines.forEach((line) => {
+    //console.log("start :" + line + ": end");
+    if (line.startsWith("\\f")) {
+      console.log(line);
+    }
+  })
 }
